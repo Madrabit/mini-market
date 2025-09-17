f0eac42aad662ac3200436a86ace5a4bb010d1d1
Madrabit <madrabot@gmail.com>
2025-09-17 11:44:56 +0300

    add validators

--- analytic/internal/service.go
+++ analytic/internal/service.go
@@ -1,1 +1,74 @@
 package internal
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/madrabit/mini-market/analytic/internal/common"
+)
+
+type Service struct {
+	repo      Repo
+	validator Validator
+}
+
+type Repo interface {
+	FindEventById(tx *sqlx.Tx, eventID uuid.UUID) (bool, error)
+	BeginTransaction() (tx *sqlx.Tx, err error)
+	AddEvent(tx *sqlx.Tx, event Event) error
+	GetOrdersSummary(from, to string) (OrdersSummary, error)
+	GetTopProducts(limit int64) (TopProducts, error)
+	GetDailySails(from, to string) (DailySales, error)
+	GetSearchTrends(limit int64) (SearchTrends, error)
+	GetFailSearch(limit int64) (FailedSearch, error)
+	GetAvgRatingByProduct(productID uuid.UUID) (ProductAvgRating, error)
+	GetTopProductsByRating(limit int64) (TopProducts, error)
+}
+
+type Validator interface {
+	Validate(request any) error
+}
+
+func NewService(repo Repo, validator Validator) *Service {
+	return &Service{repo, validator}
+}
+
+func (s *Service) Add(event Event) (err error) {
+	if err = s.validator.Validate(event); err != nil {
+		return &common.RequestValidationError{Message: err.Error()}
+	}
+	tx, err := s.repo.BeginTransaction()
+	if err != nil {
+		return fmt.Errorf("analytic service: add event: error starting transaction")
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			err = fmt.Errorf("analytic service: add event:  panic add employee: %v", p)
+			return
+		}
+		if err != nil {
+			if rbErr := tx.Rollback(); rbErr != nil {
+				err = fmt.Errorf("rollback failed: original error: %w", err)
+			}
+			return
+		}
+		if commitErr := tx.Commit(); commitErr != nil {
+			err = fmt.Errorf("analytic service: add event: committing transaction failed: %w", commitErr)
+		}
+	}()
+	isExists, err := s.repo.FindEventById(tx, event.ID)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("analytic service: add event: error checking exists of event")
+	}
+	if isExists {
+		return &common.AlreadyExistsError{Message: fmt.Sprintf("event with id %s already exists", event.ID)}
+	}
+	err = s.repo.AddEvent(tx, event)
+	if err != nil {
+		return fmt.Errorf("analytic service: add event: error adding event")
+	}
+	return nil
+}
---
+++ shipping/internal/validator/validator.go
@@ -0,0 +1,25 @@
+package validator
+
+import (
+	"errors"
+	"github.com/go-playground/validator/v10"
+)
+
+type Validator struct {
+	validator *validator.Validate
+}
+
+func New() *Validator {
+	return &Validator{validator: validator.New()}
+}
+
+func (v *Validator) Validate(request any) (err error) {
+	err = v.validator.Struct(request)
+	if err != nil {
+		var validateErrs validator.ValidationErrors
+		if errors.As(err, &validateErrs) {
+			return validateErrs
+		}
+	}
+	return err
+}
